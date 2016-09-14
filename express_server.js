663d69e1c70fde2e64b9e2da5acf347baab7b877
devin-web <devin@alumni.sfu.ca>
2016-09-14 23:01:42 +0000

    deletion working

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 var express = require("express");
 const bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded(
+{
+  extended: true
+}));
 
 var urlDatabase = {
   urls: {
@@ -18,7 +23,7 @@ function generateRandomString() {
   var returnString = "";
   var lowerCaseRE = new RegExp("[a-z]");
   for( var characterIndex in splitString ){
-    console.log( splitString[characterIndex] );
+    //console.log( splitString[characterIndex] );
      //do regex to find lower case letters
     if( splitString[characterIndex].search( lowerCaseRE ) !== -1 ){
     //randomly flip a coin to determine whether to upper case it
@@ -30,6 +35,45 @@ function generateRandomString() {
   }
   return returnString;
 }
+// app.use(methodOverride('_method'){
+// }
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+app.delete("/urls/:id", (req, res) => {
+  console.log( "Deleting:", req.params.id );
+  res.end( "Deleting OK" );
+});
+/*
+app.use( methodOverride( function(req, res, next){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    console.log( "Method:", method );
+    return method;
+  }
+  //console.log( req, "\n\n" );
+  if( req.body ){
+    console.log( "req.body:", req.body );
+    if( typeof req.body === 'object'){
+      console.log( "req.body is object" );
+      console.log(  "\n==================\n", req.body,
+                    "\n==================\n", req.header.url,
+                    "\n==================\n", req.header );
+      if( '_method' in req.body ){
+        console.log( "_method:", re.body._method );
+      }
+    }
+
+  }
+*/
+
+/* else{
+    next();
+  }*/
+//  return null;
+//}));
 
 app.set('view engine', 'ejs');
 
