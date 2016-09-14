ec0e900f8561882fb769f7ee4665001c878afb03
devin-web <devin@alumni.sfu.ca>
2016-09-14 20:01:01 +0000

    Redirection now working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+
+app.use(bodyParser.urlencoded());
 
 var urlDatabase = {
   urls: {
@@ -8,6 +11,25 @@ var urlDatabase = {
     "9sm5xK": "http://www.google.com"
   }
 };
+
+function generateRandomString() {
+  var initialString = Math.random().toString(36).substr(2,6);
+  var splitString = initialString.split("");
+  var returnString = "";
+  var lowerCaseRE = new RegExp("[a-z]");
+  for( var characterIndex in splitString ){
+    console.log( splitString[characterIndex] );
+     //do regex to find lower case letters
+    if( splitString[characterIndex].search( lowerCaseRE ) !== -1 ){
+    //randomly flip a coin to determine whether to upper case it
+      if( Math.random() >= 0.5 ){
+        splitString[characterIndex] = splitString[characterIndex].toUpperCase();
+      }
+    }
+    returnString += splitString[characterIndex];
+  }
+  return returnString;
+}
 
 app.set('view engine', 'ejs');
 
@@ -27,6 +49,34 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", urlDatabase );
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/*", (req, res) =>{
+  console.log( req.url );
+  var splitURL  = req.url.split("/");
+  var shortURL  = splitURL[splitURL.length - 1];
+  var longURL   = urlDatabase.urls[shortURL];
+  console.log(  longURL );
+  res.redirect( longURL );
+});
+
+app.post("/urls", (req, res) => {
+  var longUrl = req.body;
+  var newShortUrl = generateRandomString();
+  console.log( "new URL: ", newShortUrl, longUrl.longURL );  // debug statement to see POST parameters
+
+  while( urlDatabase.urls.hasOwnProperty( newShortUrl ) ){
+    newShortUrl = generateRandomString();
+    console.log( newShortUrl, longUrl.longURL );
+  }
+  urlDatabase.urls[newShortUrl] = longUrl.longURL;
+
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
+
